f5ea07fa0448f4a1fba81016526a9cadd9d462c1
BA Coder <bxtrabduraimov@gmail.com>
2025-02-04 10:04:33 +0300

    1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,21 @@
 /*
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800">
-      <h1 className="logo">Blog App</h1>
-      <div className="nav-link-container">
-        <Link className="nav-link" to="/">Home</Link>
-        <Link className="nav-link" to="/blog">Blog</Link>
-        <Link className="nav-link" to="/profile">Profile</Link>
-      </div>
+    <h1 className="logo">Blog App</h1>
+    <div className="nav-link-container">
+    <Link className="nav-link" to="/">Home</Link>
+    <Link className="nav-link" to="/blog">Blog</Link>
+    <Link className="nav-link" to="/profile">Profile</Link>
+    </div>
     </nav>
-  );
-};
-
-export default Navbar;
-*/
-
-
+    );
+    };
+    
+    export default Navbar;
+    */
+   
 
 const navigation = [
   { name: 'Home', href: '/', current: true },
@@ -33,32 +31,25 @@ export default function Example() {
   return (
     <nav className="navbar w-100vh">
           <div className="logo flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 items-center">
               <img
                 alt="Your Company"
                 src="https://baxtiyor-abduraim0v.vercel.app/logo.png"
                 className="h-8 w-8"
               />
-            </div>
-
-
             <div className="nav-link hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
                     aria-current={item.current ? 'page' : undefined}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium',
+                      item.current ? 'current' : 'not-current',
                     )}
                   >
                     {item.name}
                   </a>
                 ))}
               </div>
-            </div>
           </div>
     </nav>
   )
